caa0c1dc00ff7cb2a8e75c1b6ed9f2705ba93df1
François Courmont <courmont.f@sfeir.com>
2023-10-06 13:53:30 +0200

    fix: change sudo mode to run commands

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
-SUDO="sudo"
 GIT_REPOSITORY="https://github.com/courmontf/switch_hifi.git"
 INSTALL_DIR="/opt/switch_hifi"
 
@@ -12,12 +11,11 @@ if ! which sudo >/dev/null; then
 	fi
 fi
 
-$SUDO su
-git clone $REPO $INSTALL_DIR
-python -m venv $INSTALL_DIR/venv
+$SUDO git clone $REPO $INSTALL_DIR
+$SUDO python -m venv $INSTALL_DIR/venv
 cd $INSTALL_DIR/switch_hifi
 source venv/bin/activate
-pip install uvicorn[standard] fastapi RPi.GPIO
-cp switch_hifi.service /lib/systemd/system/switch_hifi.service
-systemctl daemon-reload
-systemctl enable switch_hifi.service
+$SUDO pip install uvicorn[standard] fastapi RPi.GPIO
+$SUDO cp switch_hifi.service /lib/systemd/system/switch_hifi.service
+$SUDO systemctl daemon-reload
+$SUDO systemctl enable switch_hifi.service
